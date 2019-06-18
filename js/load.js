773c783e86d4bf42d46e74df9e0dff61b79d755b
Álvaro Antonio <alvaroantonio@alvaro-antonio.local>
2019-06-18 20:26:53 -0300

    Added a message bubble indicating the page is still not finished.

--- js/load.js
+++ js/load.js
@@ -1,16 +1,29 @@
 body = document.getElementsByTagName('body')[0];
 heroImg = document.querySelector('.hero img');
 heroHeading = document.querySelector('.hero-heading');
+messageBubble = document.querySelector('#message');
 
 
 body.classList.toggle('loading');
+heroHeading.classList.add('initial-state');
+heroImg.classList.add('initial-state')
+messageBubble.classList.add('initial-state');
 
 document.addEventListener('readystatechange', function() {
   if (document.readyState === 'complete') {
     console.log('loading completed');
-    body.classList.toggle('loading');
+    body.classList.remove('loading');
 
-    heroImg.classList.add('grow');
-    heroHeading.classList.add('fade-in')
+    heroImg.classList.remove('initial-state');
+    heroHeading.classList.remove('initial-state');
+
+    document.addEventListener('wheel', function() {
+      console.log('moving');
+      messageBubble.classList.remove('initial-state');
+    });
+    document.addEventListener('touchstart', function() {
+      console.log('moving');
+      messageBubble.classList.remove('initial-state');
+    });
   }
 });
